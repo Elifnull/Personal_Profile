4d1c82262a16b26d0f451925960fa521d6a8c680
Serge Chvorun <elifnull@protonmail.com>
2022-06-02 10:58:51 -0400

    layout and routing setup"

--- Website/web/src/App.js
+++ Website/web/src/App.js
@@ -1,12 +1,16 @@
-import { Route, Routes } from 'react-router-dom'
+
+import { Routes} from 'react-router-dom'
 import './App.scss'
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        
+      </Routes>
     </>
   )
 }
 
 export default App
+//Personal Note: BrowserRouter is in the index.js file wrapping around <APP> instead of in the App.js
