cc7d1a1922bc1082a1ffde228ed51397cc8a934a
Serge Chvorun <elifnull@protonmail.com>
2022-06-06 12:45:30 -0400

    folder notes

--- Website/web/src/components/Layout/index.js
+++ Website/web/src/components/Layout/index.js
@@ -1,7 +1,8 @@
 import SideBar from '../Sidebar';
 import './index.scss';
 
-// Setup views here on layoutç
+// Setup views here on layout
+//build seperate scss files in each components subfolder (namming convention index.js and index.scss)
 const Layout = () =>{
 
 
