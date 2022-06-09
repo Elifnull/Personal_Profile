761ede495690b3d241bb9a862089ea33ecd10c0c
Serge Chvorun <elifnull@protonmail.com>
2022-06-09 10:16:53 -0400

    sidebar work

--- Website/web/src/components/Sidebar/index.js
+++ Website/web/src/components/Sidebar/index.js
@@ -1,11 +1,18 @@
 import "./index.scss";
+import {Link} from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'  //This is an import of the Logo with a names
+import LogoSub from '../../assets/images/logo_sub.png'
 
 const SideBar = () => {
 
     return(
-        <>
-        SideBar Hello!
-        </>
+        <div className='nav-bar'>
+            <Link className="logo" to="/">
+                <img src={LogoS} alt="logo"/>
+                <img src={LogoSub} alt="Serge" />
+            </Link>
+        </div>
+        
     )
 }
 
