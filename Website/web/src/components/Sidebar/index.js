4d43a4472b376518332045075cb4dfe3fb130dbc
Serge Chvorun <elifnull@protonmail.com>
2022-06-09 11:47:29 -0400

    scss syntax correction

--- Website/web/src/components/Sidebar/index.js
+++ Website/web/src/components/Sidebar/index.js
@@ -1,7 +1,10 @@
 import "./index.scss";
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'  //This is an import of the Logo with a names
 import LogoSub from '../../assets/images/logo_sub.png'
+import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const SideBar = () => {
 
@@ -11,6 +14,21 @@ const SideBar = () => {
                 <img src={LogoS} alt="logo"/>
                 <img src={LogoSub} alt="Serge" className="subLogo" />
             </Link>
+            <nav>
+                <NavLink exact="true" acctiveclassname="active" to='/'>
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" acctiveclassname="active" to='/about'
+                className="aboutLink"
+                >
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" acctiveclassname="active" to='/'
+                className='contactLink'
+                >
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
         </div>
         
     )
