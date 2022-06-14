6b179bb06ae3316d29de02cf1f4be8968673e1a5
Serge Chvorun <elifnull@protonmail.com>
2022-06-14 12:48:09 -0400

    intro description and contnent update, correction of div

--- Website/basic/src/components/Intro/Intro.jsx
+++ Website/basic/src/components/Intro/Intro.jsx
@@ -6,9 +6,21 @@ const Intro = () => {
     return (
     <div className='intro'>
         <div className="introLeft">
-            <div className="leftWrapper">
+            <div className="introLeftWrapper">
                 <h2 className='introText'>Hello, my name is</h2>
                 <h1 className='introName'>Serge Chvorun</h1>
+                <div className="introTitle">
+                    <div className="introTitleWrapper">
+                        <div className="introTitleText">Web Developer</div>
+                        <div className="introTitleText">FrontEnd Engineer</div>
+                        <div className="introTitleText">React Dev</div>
+                        <div className="introTitleText">Python Developer</div>
+                    </div>
+                </div>
+                <div className="introDescription">
+                    I create applications and design cool experiences using ReactJS and Python,
+                    both on Backend and Frontend, all while mainting my love of Vectors and Tensors.
+                </div>
             </div>
         </div>
         <div className="introRight">RightSide</div>
