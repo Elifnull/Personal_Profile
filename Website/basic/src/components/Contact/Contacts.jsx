12e43e42e70125b34a8c38949a93f7b13f88e8ef
Serge Chvorun <elifnull@protonmail.com>
2022-06-17 19:39:59 -0400

    syntax correction for contacts component

--- Website/basic/src/components/Contact/Contacts.jsx
+++ Website/basic/src/components/Contact/Contacts.jsx
@@ -6,13 +6,23 @@ const Contacts = () => {
         <div className='contactsPage'>
             <div className="contactBackground"></div>
             <div className="contactWrapper">
-                <div className="contactsLeft"></div>
-                leftSide
-                <div className="contactsRight"></div>
-                RightSide
+                <div className="contactsLeft">
+                    <h1 className="contactsTitle">lets discussprojects</h1>
+                    <div className="contactInfo">
+                        <div className="contactInfoItem">
+                            <img src='' alt='' className='contactInfoIMG'/>
+                        </div>
+                    </div>
+                </div>
+                <div className="contactsRight">
+                <p>Side</p>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Contacts
+
+
+//css issue turned out to be a <div> issue. note!!!! 
